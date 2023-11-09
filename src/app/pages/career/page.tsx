52f59472c226d32f8c4d00c4e8e28efce392769e
Laure <dominguezlaure@gmail.com>
2023-11-09 23:19:46 +0100

    complited toggle display and filter for timeline page

--- src/app/pages/career/page.tsx
+++ src/app/pages/career/page.tsx
@@ -3,49 +3,95 @@ import jobsData from '@/app/public/jobs.json';
 import educData from '@/app/public/educ.json';
 import { useState } from 'react';
 import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faUserTie } from '@fortawesome/free-solid-svg-icons';
+
 
 const Career = () => {
 	const combinedData: Array<any> = [...jobsData, ...educData];
-	const [active, setActive] = useState(-1);
+
+	const getEndDate = (date: string) => {
+			if (date.includes('Depuis')) {
+				return Number.POSITIVE_INFINITY;
+		}
+		const dateParts = date.split(' - ');
+		if (dateParts.length > 1) {
+			const endYear = parseInt(dateParts[1], 10);
+			return isNaN(endYear) ? Number.NEGATIVE_INFINITY : endYear;
+		} else {
+			return parseInt(dateParts[0], 10) || Number.NEGATIVE_INFINITY;
+		}
+		};
+
+		const combinedDataSorted = combinedData.sort(
+			(a, b) => getEndDate(b.date) - getEndDate(a.date)
+		);
+
+	const [activeElements, setActiveElements] = useState(
+		Array(combinedDataSorted.length).fill(false)
+	);
 
 	const handleArticleClick = (index: number) => {
-		setActive(index);
-	}
+		const newActiveElements = [...activeElements];
+		newActiveElements[index] = !newActiveElements[index];
+		setActiveElements(newActiveElements);
+	};
 
 	return (
 		<main>
 			<div className="container career">
 				<h1>Mon parcours professionnel :</h1>
 				<div className="timeline">
-					{combinedData.map((item, index) => (
+					{combinedDataSorted.map((item, index) => (
 						<article
 							key={index}
 							className={classNames({
 								jobs: item.hasOwnProperty('company'),
 								educ: !item.hasOwnProperty('company'),
-								active: index === active,
+								active: activeElements[index],
 							})}
 							onClick={() => handleArticleClick(index)}
 						>
-							<h3>
-								{item.date}
-								{item.hasOwnProperty('duration') && item.duration !== null
-									? ' | ' + item.duration
-									: ''}
-								{' | ' + item.title}
-								{item.hasOwnProperty('company') ? ' | ' + item.company : ''}
-								{item.hasOwnProperty('degree') && item.degree !== null ? (
-									<span className="degree">{', ' + item.degree}</span>
-								) : (
-									''
-								)}
-								{item.hasOwnProperty('school') && (
-									<span className="school">
-										<br />
-										{item.school}
-									</span>
-								)}
-							</h3>
+							<div className="headersTimeline">
+								<FontAwesomeIcon
+									icon={
+										item.hasOwnProperty('company') ? faUserTie : faGraduationCap
+									}
+								/>
+								<h3>
+									<span className="title">{item.title}</span>
+									{item.hasOwnProperty('company') ? (
+										<span className="company">
+											<br />
+											{item.company}
+										</span>
+									) : (
+										''
+									)}
+									{item.hasOwnProperty('degree') && item.degree !== null ? (
+										<span className="degree">{', ' + item.degree}</span>
+									) : (
+										''
+									)}
+									{item.hasOwnProperty('school') && (
+										<span className="school">
+											<br />
+											{item.school}
+										</span>
+									)}
+								</h3>
+								<div>
+									<span className="date">{item.date}</span>
+									{item.hasOwnProperty('duration') && item.duration !== null ? (
+										<span className="duration">
+											<br />
+											{' ' + item.duration}
+										</span>
+									) : (
+										''
+									)}
+								</div>
+							</div>
 							{item.description && (
 								<ul>
 									{item.description.map((desc: string, descIndex: number) => (
